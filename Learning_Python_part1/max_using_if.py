f681c18dbb540ef000fc9adcdd9ff9ad12f95889
c c sreenidhin <ccsreenidhin@gmail.com>
2017-04-17 12:20:00 +0530

    workshop

---
+++ Learning_Python_part1/max_using_if.py
@@ -0,0 +1,9 @@
+a = 2
+b = 4
+
+if a>b:
+    print (a)
+
+else:
+    print ("Max" ,b)
+
---
+++ Learning_Python_part1/tk1.py
@@ -0,0 +1,7 @@
+from Tkinter import *
+
+top = Tk()
+img = PhotoImage(file="C:\hh\python-512.gif")
+w= Label(top, image = img)
+w.pack(side ="right")
+top.mainloop()
